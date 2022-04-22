777840b402aa0e80fe18ba96fdc5b1bc61304217
Guillaume Clement <guillaume.clement@aftia.com>
2022-04-21 21:19:38 -0600

    Adding query string based fetching

---
+++ js/fetch.js
@@ -0,0 +1,24 @@
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+let formRef = params.form; // "some_value"
+
+var morfRoot = 'https://render.getmorf.io';
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", formRef, true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+
+xhr.onreadystatechange = function () {
+    if (this.readyState != 4) return;
+
+    if (this.status == 200) {
+        var morf = JSON.parse(this.responseText)
+    }
+    else {
+        console.error("Error occured") //TODO
+    }    
+};
+
+xhr.send();
