59e18d429ef3d71c0091806dfff040a562c34852
Guillaume Clement <guillaume.clement@aftia.com>
2022-04-25 15:17:45 -0600

    Update fetch.js logic

--- js/fetch.js
+++ js/fetch.js
@@ -2,26 +2,33 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
 
-let formRef = params.form; // "some_value"
+let formRef = params.form;
 
 var morfRoot = 'https://render.getmorf.io';
 
-const xhr = new XMLHttpRequest();
-xhr.open("GET", formRef, true);
-xhr.setRequestHeader('Content-Type', 'application/json');
+if (formRef){
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", formRef, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
 
-xhr.onreadystatechange = function () {
-    if (this.readyState != 4) return;
+    xhr.onreadystatechange = function () {
+        if (this.readyState != 4) return;
 
-    if (this.status == 200) {
-        window.morf = JSON.parse(this.responseText);
-        var embed = document.createElement('script');
-        embed.setAttribute('src','https://render.getmorf.io/js/embed.js');
-        document.body.appendChild(embed);
-    }
-    else {
-        console.error("Error occured") //TODO
-    }    
-};
+        if (this.status == 200) {
+            window.morfConfig = {
+                root: morfRoot,
+                targetId: 'morfcontent',
+                definition: JSON.parse(this.responseText)
+            };
 
-xhr.send();
+            var embed = document.createElement('script');
+            embed.setAttribute('src', morfRoot + '/js/embed.js');
+            document.body.appendChild(embed);
+        }
+        else {
+            console.error("Error occured") //TODO
+        }    
+    };
+
+    xhr.send();
+}
